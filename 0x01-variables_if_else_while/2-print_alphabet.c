a2eccf394e4f31e1f2f4c9eb6f8fa4c936726d57
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-17 04:56:19 -0700

    push fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-/*
- * the msin function is to print out all lower case letters
+/**
+ * main - Entry point of the program
+ *
+ * The program prints out all the lower case alphabet.
+ * It uses the putchar function.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alp = 'a';
+
 	while (alp <= 'z')
 	{
 		putchar(alp);
 
 		alp++;
-		
 	}
 	putchar('\n');
 	return (0);
