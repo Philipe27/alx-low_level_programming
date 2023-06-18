40e01f3564149dd3de8fdd4b128bbefb7d0eb519
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-18 09:20:44 -0700

    not comipling

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the start of the programme
+ *
+ * This programmeis expectexd to print out all sinhle digits from 0
+ *
+ * Return: Always 0 (Sucsess)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+
+		num++;
+		printf('\n');
+	}
+	return(0);
+}
