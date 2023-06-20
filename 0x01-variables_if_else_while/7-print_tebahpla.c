3b14474c185325852c772d718953eb201ef65d2b
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-19 21:43:34 -0700

    push now

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main: This is the entty point of programme
+ *
+ * This programme prints out the lower cae letter with put char.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    char letter = 'z';
+
+    while (letter >= 'a')
+    {
+        putchar(letter);
+        letter--;
+    }
+
+
+    putchar('\n');
+
+    return 0;
+}
