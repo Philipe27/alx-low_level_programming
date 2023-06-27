54461952e792bdbb8ef6b31ac3461275eb9097ff
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-27 10:12:16 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This the entry point of tye function
+ *
+ * This function prints out the _putchar ffunction to console
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	char apl = 'a'
+		while(apl <= 'z')
+		{
+			putchar(alp);
+			apl++;
+		}
+	putchar('\n');
+	return (0);
+}
