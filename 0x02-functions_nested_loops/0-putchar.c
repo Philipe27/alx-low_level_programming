06e8bd3adc2628065cb741e485bccbdabc65aeea
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-27 12:32:47 -0700

    push 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - This the entry point of tye function
+ *
+ * This function prints out the _putchar ffunction to console
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+
+	return(0);
+}
