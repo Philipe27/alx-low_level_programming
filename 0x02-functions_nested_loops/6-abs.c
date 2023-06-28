ccd3ee3b32b6886fbf3d05750ebe32ae8d334a08
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-28 11:18:24 -0700

    push tsk 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "mai.h"
+/**
+ * _abs - This is the start of the function
+ * @n: Is a place holder
+ *
+ * This functin returns the absolute value of n
+ *
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
