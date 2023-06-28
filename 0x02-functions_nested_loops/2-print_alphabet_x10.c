a8e9d1d203ba2dabb685653c4db5c0f745021180
Adegoke Philip <philipadegoke04@yahoo.com>
2023-06-28 03:51:38 -0700

    push 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "header.h"
+/**
+ * print_alphabet_x10- this is the start of tge function 
+ *
+ * This function prints the lower case alphabet ten times
+ *
+ * Return: Always sucsess
+ */
+void print_alphabet_x10(void);
+{
+	int out = 0;
+	char ltt = 'a';
+
+	while (out < 10)
+	{
+		while (ltt <= 'z')
+		{
+			_putchar(ltt);
+			ltt++;
+		}
+		out++;
+	}
+	_putchar('\n');
+}
